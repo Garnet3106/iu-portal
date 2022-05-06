6f900c874a91a724881c1a217c6ecd7121c5eccd
Garnet3106 <manage@gant.work>
2022-05-06 13:25:10 +0900

    add: assignment list layout

--- src/App/AssignmentList/AssignmentList.tsx
+++ src/App/AssignmentList/AssignmentList.tsx
@@ -6,7 +6,41 @@ function AssignmentList() {
         <>
         <Header />
         <div className="AssignmentList">
-            AssignmentList
+            <div className="display-order">
+                <div className="display-order-item">
+                    期限が早い順
+                </div>
+                <div className="display-order-item">
+                    出題が早い順
+                </div>
+            </div>
+            <hr className="division-line" />
+            <div className="assignment-list">
+                <div className="assignment-group">
+                    <div className="assignment-group-title">
+                        提出期限切れ
+                    </div>
+                    <div className="assignment-group-content">
+                        <div className="assignment-item">
+                            <div>
+                                <div className="assignment-item-operation" />
+                                <div className="assignment-item-content">
+                                    <div className="assignment-item-subject">
+                                        マーケティング基礎
+                                    </div>
+                                    <div className="assignment-item-deadline">
+                                        今週日曜日 23:59 まで
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="assignment-item-button-list">
+                                <div className="assignment-item-button-item" />
+                                <div className="assignment-item-button-item" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     );
