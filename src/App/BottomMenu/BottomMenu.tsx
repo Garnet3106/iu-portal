01d1977d06fab76c8bb7355c10fcb7793cf6a392
Garnet3106 <manage@gant.work>
2022-05-06 14:11:28 +0900

    add: bottom menu layout

--- src/App/BottomMenu/BottomMenu.tsx
+++ src/App/BottomMenu/BottomMenu.tsx
@@ -3,7 +3,16 @@ import './BottomMenu.css';
 function BottomMenu() {
     return (
         <div className="BottomMenu">
-            BottomMenu
+            <div className="menu">
+                <div className="menu-item" />
+                <div className="menu-item" />
+                <div className="menu-item" />
+                <div className="menu-item" />
+                <div className="menu-item" />
+            </div>
+            <div className="menu-bar-area">
+                <div className="menu-bar" />
+            </div>
         </div>
     );
 }
