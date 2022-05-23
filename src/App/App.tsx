8ce8d8bccef834212478b7276d0cc4f655bc859a
Garnet3106 <manage@gant.work>
2022-05-23 19:49:42 +0900

    fix: response typos

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -14,13 +14,13 @@ import './App.css';
 // req.addEventListener('load', () => {
 //     console.log(req.responseText);
 
-//     if (responce.status === 'error') {
-//         let msg = responce.message;
+//     if (response.status === 'error') {
+//         let msg = response.message;
 //         console.error(`Assignment Error: Failed to load assignments. (${msg !== '' ? msg : 'no message'})`);
 //         return;
 //     }
 
-//     AppDispatcher.dispatch(UiActionCreators.initializeAssignments(responce.content.assignments));
+//     AppDispatcher.dispatch(UiActionCreators.initializeAssignments(response.content.assignments));
 // });
 
 // req.open('GET', `https://iu-portal.gant.work/api.php?request=${encodeURIComponent(`{"action":"get_asgn"}`)}`);
@@ -33,8 +33,8 @@ class App extends React.Component<{}> {
     constructor(props: {}) {
         super(props);
 
-        let responce = JSON.parse(`{"status":200,"request":"{\\"action\\":\\"getAssignments\\",\\"includeCompleted\\":true}","contents":{"assignments":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"registrarId":"3db893b5-d247-11ec-8085-49bfe3345a29","numberOfCheckers":2,"courseId":"3db893b5-d247-11ec-8085-49bfe3345a29","lectureId":"3db893b5-d247-11ec-8085-49bfe3345a29","assignedFrom":"3db893b5-d247-11ec-8085-49bfe3345a29","assignedFromLink":null,"submitTo":"3db893b5-d247-11ec-8085-49bfe3345a29","submitToLink":null,"deadline":"2022-05-14 11:05:02","description":"desc","note":"notes","completed":false}},"courses":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"code":"MK11220002","name":"経営学","electionKind":"required","numberOfCredits":2,"academicYear":2022,"grade":1,"semester":"first","teacherIds":["3db893b5-d247-11ec-8085-49bfe3345a29"]}},"lectures":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"numberOfTimes":10,"date":"2022-05-14"}},"teachers":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"name":"教員太郎"}},"platforms":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"numberOfTimes":2,"nickname":"unipa"}},"users":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"nickname":"ユーザ太郎"}}}}`);
-        let assignments = apiResponseToAssignments(toAssignmentStructureApiResponse(responce));
+        let response = JSON.parse(`{"status":200,"request":"{\\"action\\":\\"getAssignments\\",\\"includeCompleted\\":true}","contents":{"assignments":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"registrarId":"3db893b5-d247-11ec-8085-49bfe3345a29","numberOfCheckers":2,"courseId":"3db893b5-d247-11ec-8085-49bfe3345a29","lectureId":"3db893b5-d247-11ec-8085-49bfe3345a29","assignedFrom":"3db893b5-d247-11ec-8085-49bfe3345a29","assignedFromLink":null,"submitTo":"3db893b5-d247-11ec-8085-49bfe3345a29","submitToLink":null,"deadline":"2022-05-14 11:05:02","description":"desc","note":"notes","completed":false}},"courses":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"code":"MK11220002","name":"経営学","electionKind":"required","numberOfCredits":2,"academicYear":2022,"grade":1,"semester":"first","teacherIds":["3db893b5-d247-11ec-8085-49bfe3345a29"]}},"lectures":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"numberOfTimes":10,"date":"2022-05-14"}},"teachers":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"name":"教員太郎"}},"platforms":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"numberOfTimes":2,"nickname":"unipa"}},"users":{"3db893b5-d247-11ec-8085-49bfe3345a29":{"nickname":"ユーザ太郎"}}}}`);
+        let assignments = apiResponseToAssignments(toAssignmentStructureApiResponse(response));
         AppDispatcher.dispatch(UiActionCreators.initializeAssignments(assignments));
     }
 
